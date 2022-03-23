126b72c9a0d3fdafbf1f5b8198beb2e3081abb6d
Eralp Zeydan <eralpzeydan@gmail.com>
2022-03-23 23:33:20 +0300

    Temporary animations

---
+++ DashGame/Assets/CharacterMovement/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Vector3 lastMoveDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        HandlerMovement();
+    }
+
+    private void HandlerMovement(){
+        float speed = 40f;
+        float moveX = 0f;
+        float moveY = 0f;
+
+        if(Input.GetKey(KeyCode.W)){
+            moveY = +1f;
+        }
+        if(Input.GetKey(KeyCode.S)){
+            moveY = -1f;
+        }
+        if(Input.GetKey(KeyCode.A)){
+            moveX = -1f;
+        }
+        if(Input.GetKey(KeyCode.D)){
+            moveX = +1f;
+        }
+
+        Vector3 moveDirection = new Vector3(moveX,moveY).normalized;
+        transform.position += moveDirection*speed*Time.deltaTime;
+
+    }
+}
